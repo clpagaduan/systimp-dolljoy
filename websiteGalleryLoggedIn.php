23bcad645dff42d9eefc36ed7090f7a51665e160
clpagaduan <37362629+clpagaduan@users.noreply.github.com>
2018-07-08 12:29:37 +0800

    added this line

--- websiteGalleryLoggedIn.php
+++ websiteGalleryLoggedIn.php
@@ -7,7 +7,7 @@ if(!isset($_SESSION)) {
 
 $username = $_SESSION['username'];
 //echo $username;
-
+//added this line
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
