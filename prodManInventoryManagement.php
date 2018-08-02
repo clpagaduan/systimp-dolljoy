3ac29dcb5818b58c507003b2d26e385ada1e81c3
Kourt <mirandakourt@naver.com>
2018-08-02 23:48:03 +0800

    Modal Help
    still need help on the modal in here i'm trying

--- prodManInventoryManagement.php
+++ prodManInventoryManagement.php
@@ -433,30 +433,30 @@ if (!empty($sql))
 </div>
 </div>
     
-    <div class="modal fade" id="receiveModal" tabindex="-1"  role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Inventory Management</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body"> Confirm RECEIVE supply?
-      </div>
-      <div class="modal-footer">
-          <button type="submit" name="accept" onclick="Alert()" class="btn btn-secondary">CONFIRM</button>
-        <button type="submit" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-      </div>
-    </div>
-  </div>
-</div>
+  
      
                 
             
 </form>
 
-
+  <div class="modal fade" id="receiveModal" tabindex="-1"  role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    
+    <div class="modal-content">
+     
+      <div class="modal-body"> 
+            Confirm RECEIVE supply?
+      </div>
+      <div class="modal-footer">
+          
+            <button type="submit" name="accept" onclick="Alert()" class="btn btn-secondary">  CONFIRM
+            </button>
+            <button type="submit" class="btn btn-secondary" data-dismiss="modal">   CANCEL
+            </button>
+      </div>
+    </div>
+  </div>
+</div>
     
             
 
