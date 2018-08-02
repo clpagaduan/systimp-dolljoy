dad2dc37b4057e9464fb8fde37445cf874b91d39
Kourt <mirandakourt@naver.com>
2018-08-02 23:40:41 +0800

    Current Orders | Review Orders
    added a search and sort feature

--- prodManCurrentOrders.php
+++ prodManCurrentOrders.php
@@ -216,6 +216,10 @@ if(isset($_POST['reject'])){
 
 
         <div class="content">
+
+            <form class="form-horizontal" method = "post" action = "<?php echo $_SERVER['PHP_SELF']?>">
+
+
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-12">
@@ -225,18 +229,22 @@ if(isset($_POST['reject'])){
                             </div>
                             <br><br>
 
+                            
+
                                 <div class="content table-responsive table-full-width">
                                 
-                                <table class="table table-hover">
-                                    <thead>
-                                        <th><p class="category"><b>ORDER ID</b></p></th>
-                                       
-                                        <th><p class="category"><b>DATE ORDERED</b></p></th>
-                                         <th><p class="category"><b>CLIENT</b></p></th>
-                                        <th><p class="category"><b>PAYMENT STATUS</b></p></th>
-                                        <th><p class="category"><b>MANUFACTURING STATUS</b></p></th>
+                                   <input id="myInput" type="search" onkeyup="search();" name = "searchOrder" class="form-control col-sm-2" placeholder="Search Order..."> 
 
-                                        <th><p class="category"><b>DATE REQUIRED</b></p></th>
+                                <table  id="myTable" class="table table-hover">
+                                    <thead>
+                                        <th onclick="sortTable(0)"><p class="category"><b>ORDER ID</b></p></th>
+                                       
+                                        <th onclick="sortTable(1)"><p class="category"><b>DATE ORDERED</b></p></th>
+                                         <th onclick="sortTable(2)" ><p class="category"><b>CLIENT</b></p></th>
+                                        <th onclick="sortTable(3)"><p class="category"><b>PAYMENT STATUS</b></p></th>
+                                        <th onclick="sortTable(4)" ><p class="category"><b>MANUFACTURING STATUS</b></p></th>
+
+                                        <th onclick="sortTable(5)"><p class="category"><b>DATE REQUIRED</b></p></th>
                                     </thead>
 <?php
 
@@ -317,7 +325,86 @@ echo "
 
     </div>
 </div>
+<script>
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myTable");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+    
+    function search() {
+        // Declare variables 
+        var input, filter, table, tr, td, i;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+        th = table.getElementsByTagName("th");
 
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 1; i < tr.length; i++) {
+            tr[i].style.display = "none";
+            for (var j = 0; j < th.length; j++) {
+                td = tr[i].getElementsByTagName("td")[j];
+                if (td) {
+                    if (td.innerHTML.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
+                        tr[i].style.display = "";
+                        break;
+                    }
+                }
+            }
+        }
+    }
+</script>
 
 </body>
 
